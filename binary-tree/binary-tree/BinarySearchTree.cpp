69f2eb55820dbf83251e806bbd6ad000caf63559
Wojciech Fiołka <56364007+Kabanosk@users.noreply.github.com>
2021-04-03 00:50:35 +0200

    Add files via upload

---
+++ binary-tree/binary-tree/BinarySearchTree.cpp
@@ -0,0 +1,66 @@
+﻿#include "BinarySearchTree.h"
+
+BinarySearchTree::BinarySearchTree() {
+	root = NULL;
+}
+
+
+void BinarySearchTree::destroyNode(Node* node) {
+	if (node != NULL) {
+		destroyNode(node->left);
+		destroyNode(node->right);
+		delete node;
+	}
+}
+
+void BinarySearchTree::destroyTree() {
+	destroyNode(root);
+}
+
+
+void BinarySearchTree::insert(int key) {
+	if (root == NULL) {
+		root = new Node(key);
+	}
+	else {
+		insert(key, root);
+	}
+}
+
+void BinarySearchTree::insert(int key, Node* node) {
+	if (key >= node->data) {
+		if (node->right == NULL) 
+			node->right= new Node(key);
+		else 
+			insert(key, node->right);
+	}
+	else {
+		if (node->left == NULL) 
+			node->left = new Node(key);
+		else
+			insert(key, node->left);
+	}
+}
+
+
+Node* BinarySearchTree::search(int key) {
+	return search(key, root);
+}
+
+Node* BinarySearchTree::search(int key, Node* node) {
+	if (node->data == NULL) {
+		return NULL;
+	}
+	else {
+		if (node->data == key) {
+			return node;
+		}
+		else if (key > node->data) {
+			return search(key, node->right);
+		}
+		else {
+			return search(key, node->left);
+		}
+	}
+}
+
---
+++ binary-tree/binary-tree/BinarySearchTree.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int data;
+	struct Node* left;
+	struct Node* right;
+
+	Node(int data) {
+		this->data = data;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+class BinarySearchTree
+{
+public:
+	BinarySearchTree();
+	
+	void insert(int val);
+	Node* search(int val);
+	void destroyTree();
+
+private:
+	Node* root;
+	void destroyNode(Node* node);
+	void insert(int val, Node* node);
+	Node* search(int val, Node* node);
+};
+
